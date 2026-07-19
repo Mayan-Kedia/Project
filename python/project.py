06ef82fa18db1cd433168514649c499fe3be543a
Mayan-Kedia <mayan@users.noreply.github.com>
2026-07-19 22:32:24 +0530

    Remove unused scratch scripts

--- python/project.py
+++
@@ -1,9 +0,0 @@
-import re
-
-html_scraped_data = "<p>Welcome to our site!</p> Click <a href='/link'>here</a> to download the document.<br><b>Thank you!</b>"
-
-# Find anything starting with < and ending with > and delete it 
-cleaned_html =re.sub(r'<.*?>', '', html_scraped_data)
-
-print("Before:", html_scraped_data) 
-print("After :", cleaned_html)
